db525cdd7c31710e7cfac35c26e82236fcf3a87c
LifeToHell <120266818+LifeToHell@users.noreply.github.com>
2023-01-22 15:50:38 +0400

    Add files via upload

---
+++ task7.3.cs
@@ -0,0 +1,52 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Наш массив");
+int [,] number = new int [n,m];
+RandomNumber(number);
+void RandomNumber(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+PrintRandomNumber(number);
+void PrintRandomNumber(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
+for (int j = 0; j < number.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < number.GetLength(0); i++)
+    {
+        avarage = (avarage + number[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+
+
